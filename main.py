cc8712c47802c056afb329f809aa411e9902f635
chongjiahao <123456>
2025-01-06 11:14:51 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from sender.aliyun_sender import AliyunSmsSender
+from utils import generate_sms_code
+
+# 设置 Aliyun 的配置参数（通常这些会放在 settings配置中）
+ALIYUN_ACCESS_KEY_ID = ''  # 替换为你的阿里云 Access Key ID
+ALIYUN_ACCESS_KEY_SECRET = ''  # 替换为你的阿里云 Access Key Secret
+ALIYUN_SMS_SIGN_NAME = ''  # 短信签名
+ALIYUN_SMS_TEMPLATE_CODE = ''  # 短信模板代码
+
+# 实例化 AliyunSmsSender 对象
+# sms_sender = AliyunSmsSender(ALIYUN_ACCESS_KEY_ID, ALIYUN_ACCESS_KEY_SECRET, ALIYUN_SMS_SIGN_NAME)
+# phone_number = '15531876188'
+# res = sms_sender.send_sms(phone_number, ALIYUN_SMS_TEMPLATE_CODE, generate_sms_code())
+# print(res)
---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sms_sender_package",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "aliyun-python-sdk-core",
+        "aliyun-python-sdk-dysmsapi",
+    ],
+    description="发送短信包",
+    author="CJH",
+    author_email="942130598@qq.com",
+    url="https://github.com/chongjiahao223/python-sms_sender.git"
+)
