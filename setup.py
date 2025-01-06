a516a751358a7deacca9b331f4e1c73766741842
chongjiahao <123456>
2025-01-06 14:41:00 +0800

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sms_sender_package",
+    name="sms_sender_cjh",
     version="0.1",
     packages=find_packages(),
     install_requires=[
